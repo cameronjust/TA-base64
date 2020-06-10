ab22e0c620f4de95e279c507b487e4295dd46ef0
Cameron Just <cameron.just@rivium.com.au>
2020-06-10 11:26:11 +1000

    Creation of Git repo

---
+++ bin/b64.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import sys, os, math
+from base64 import b64decode, b64encode
+
+splunkhome = os.environ['SPLUNK_HOME']
+sys.path.append(os.path.join(splunkhome, 'etc', 'apps', 'TA-base64', 'lib'))
+from splunklib.searchcommands import dispatch, StreamingCommand, Configuration, Option, validators
+from splunklib.six.moves import range
+
+
+@Configuration()
+
+class B64Command(StreamingCommand):
+	"""
+	Encode a string to Base64
+	Decode a Base64 content
+
+	 | base64 [action=(encode|decode)] field=<field> [mode=(replace|append)] [special_chars=(keep|remove|hash)]
+	 """
+
+	field  = Option(name='field',  require=True)
+	action = Option(name='action', require=False, default="encode")
+	mode   = Option(name='mode',   require=False, default="replace")
+	special_chars = Option(name='special_chars',   require=False, default="keep")
+	convert_newlines = Option(name='convert_newlines', require=False, default=True, validate=validators.Boolean())
+	fix_padding = Option(name='fix_padding', require=False, default=True, validate=validators.Boolean())
+	suppress_error = Option(name='suppress_error', require=False, default=False, validate=validators.Boolean())
+
+	def stream(self, events):
+		module = sys.modules['base64']
+
+		if self.action == "decode" :
+			fct = "b64decode"
+		else:
+			fct = "b64encode"
+
+		if self.mode == "append" :
+			dest_field = "base64"
+		else:
+			dest_field = self.field
+
+		for event in events:
+			
+			if not self.field in event :
+				continue
+
+			try:
+			
+				# Fix padding
+				if self.fix_padding:
+					original = event[self.field].ljust((int)(math.ceil(len(event[self.field]) / 4)) * 4, '=')
+				else:
+					original = event[self.field]
+				
+				ret = getattr(module, fct)( original )
+
+				# replace unpritable characters by their hexadecimal 
+				# representation. Example: \x00
+				event[ dest_field ] = ""
+				for c in ret:
+				
+					x = c
+					if c < ord(' ') or c > ord('~') :
+						if self.convert_newlines and c == 10:
+							x = "\n"
+						elif self.convert_newlines and c == 13:
+							x = "\r"
+						elif self.special_chars == "remove":
+							continue
+						elif self.special_chars == "hash":
+							x = "#"
+						elif self.special_chars == "star":
+							x = "*"
+						else:
+							x = "\\x" + "{0:02x}".format(c)
+					else:
+						x = chr(x)
+						
+					event[ dest_field ] += x
+	
+			except Exception as e:
+				if not self.suppress_error :
+					raise e
+
+			yield event
+
+dispatch(B64Command, sys.argv, sys.stdin, sys.stdout, __name__)
+
